7ea0a1b683ae1454ad037bb9e3fb55e9773950ff
LORETOFUENTES <lfuentes.entergrafic@gmail.com>
2021-08-11 18:10:24 -0400

    creacion de routes tasks, a task index

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  Rails.application.routes.draw do
+    get    "tasks",          to: "task#index"
+  
+  #  get    "tasks/new",      to: "tasks#new",  as: :new_tasks
+  #  post   "tasks",          to: "tasks#create"
+  
+  # NB: The `show` route needs to be *after* `new` route.
+  #  get    "tasks/:id",      to: "tasks#show", as: :task
+  
+  #  get    "tasks/:id/edit", to: "task#edit", as: :edit_task
+  #  patch  "tasks/:id",      to: "tasks#update"
+  
+  #  delete "tasks/:id",      to: "tasks#destroy"
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
