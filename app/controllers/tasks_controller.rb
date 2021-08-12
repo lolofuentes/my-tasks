db7915594a9d00ee734b67c7e3392d2cea44b72f
LORETOFUENTES <lfuentes.entergrafic@gmail.com>
2021-08-12 18:10:10 -0400

    realicé los metodos del controller task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,38 @@
 class TaskController < ApplicationController
 end
+
+def index
+    @restaurants = Restaurant.all
+end
+
+def show
+    @task = Task.find(params[:id])
+end
+
+def new
+    @task = Task.new # needed to instantiate the form_for
+end
+
+def create
+    @task = Task.new(params[:task])
+    @task.save
+end
+
+def edit
+    @task = Task.find(params[:id])
+end
+
+def update
+    @task = Task.find(params[:id])
+    @task.update(params[:task])
+    # Will raise ActiveModel::ForbiddenAttributesError
+end
+
+def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    # no need for app/views/task/destroy.html.erb
+    redirect_to restaurants_path
+end
+
