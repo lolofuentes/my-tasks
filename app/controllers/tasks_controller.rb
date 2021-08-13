02d2d60b50f4d3680ced70fc8666027fdb951a8a
LORETOFUENTES <lfuentes.entergrafic@gmail.com>
2021-08-13 07:58:27 -0400

    modificacion de html ul estaba mal

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,42 +1,45 @@
 class TasksController < ApplicationController
-end
 
-def index
-    @task = Task.all
-end
+    def new
+        @restaurant = Restaurant.new # needed to instantiate the form_for
+    end
 
-def show
-    @task = Task.find(params[:id])
-end
+    def create
+        @task = Task.new(params[:task])
+        @task.save
+        redirect_to tasks_path(@task)
+    end
 
-def new
-    @task = Task.new # needed to instantiate the form_for
-end
+    def index
+        @task = Task.all
+    end
 
-def create
-    @task = Task.new(params[:task])
-    @task.save
-end
+    def edit
+        @task = Task.find(params[:id])
+    end
 
-def edit
-    @task = Task.find(params[:id])
-end
+    def show
+        @task = Task.find(params[:id])
+    end
 
-def update
-    @task = Task.find(params[:id])
-    @task.update(params[:task])
-    # Will raise ActiveModel::ForbiddenAttributesError
-end
+    def update
+        @task = Task.find(params[:id])
+        @task.update(params[:task])
+        # Will raise ActiveModel::ForbiddenAttributesError
+        redirect_to tasks_path(@task)
+    end
 
-def destroy
-    @task = Task.find(params[:id])
-    @task.destroy
-    redirect_to tasks_path
-    # no need for app/views/task/destroy.html.erb
-end
+    def destroy
+        @task = Task.find(params[:id])
+        @task.destroy
+        redirect_to tasks_path
+        # no need for app/views/task/destroy.html.erb
+    end
 
-private
+    private
 
-def params_tasks
-    params.require(:task)permit(:tittle :details :completed)
+    def params_tasks
+        params.require(:task)permit(:tittle :details :completed)
+    end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,18 @@
 Rails.application.routes.draw do
-  Rails.application.routes.draw do
 
-    get    "tasks",          to: "task#index"
-    post   "tasks",          to: "tasks#create"
+  get    "tasks",          to: "task#index"
+   
+  get    "tasks/new",      to: "tasks#new",  as: :new_tasks
+  post   "tasks",          to: "tasks#create"
   
-    get    "tasks/new",      to: "tasks#new",  as: :new_tasks
-    
-    get    "tasks/:id/edit", to: "task#edit", as: :edit_task
   # NB: The `show` route needs to be *after* `new` route.
-    get    "tasks/:id",      to: "tasks#show", as: :task
+  get    "tasks/:id",      to: "tasks#show", as: :task
   
-    patch  "tasks/:id",      to: "tasks#update"
+  get    "tasks/:id/edit", to: "task#edit", as: :edit_task
+  patch  "tasks/:id",      to: "tasks#update"
   
-    delete "tasks/:id",      to: "tasks#destroy"
-  end
+  delete "tasks/:id",      to: "tasks#destroy"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  
 end
