8948ca285134d34ff0ff37cf940a53c2838c3467
LORETOFUENTES <lfuentes.entergrafic@gmail.com>
2021-08-13 07:13:31 -0400

    modificación orden routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   Rails.application.routes.draw do
-    
+
     get    "tasks",          to: "task#index"
-  
-    get    "tasks/new",      to: "tasks#new",  as: :new_tasks
     post   "tasks",          to: "tasks#create"
   
+    get    "tasks/new",      to: "tasks#new",  as: :new_tasks
+    
+    get    "tasks/:id/edit", to: "task#edit", as: :edit_task
   # NB: The `show` route needs to be *after* `new` route.
     get    "tasks/:id",      to: "tasks#show", as: :task
   
-    get    "tasks/:id/edit", to: "task#edit", as: :edit_task
     patch  "tasks/:id",      to: "tasks#update"
   
     delete "tasks/:id",      to: "tasks#destroy"
