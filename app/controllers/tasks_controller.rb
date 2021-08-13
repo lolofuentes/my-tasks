071768e57d5df59873d9e7f0c1c21749e61028b5
LORETOFUENTES <lfuentes.entergrafic@gmail.com>
2021-08-13 12:53:42 -0400

    creacion del set e task controller y lo private, render form para new y edit, y agregacion del destroy o delete al show

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+    before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
     
     def index
         @task = Task.all
@@ -9,28 +10,28 @@ class TasksController < ApplicationController
     end
 
     def create
-        @task = Task.new(params[:task])
-        @task.save
-        redirect_to tasks_path(@task)
+    #   @task = Task.new(params[:task]).save
+        @task = Task.create(params[:task]).save
+        redirect_to tasks_path
     end
  
     def edit
-        @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
     end
 
     def show
-        @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
     end
 
     def update
-        @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
         @task.update(params[:task])
         # Will raise ActiveModel::ForbiddenAttributesError
-        redirect_to tasks_path(@task)
+        redirect_to task_path(@task)
     end
 
     def destroy
-        @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
         @task.destroy
         redirect_to tasks_path
         # no need for app/views/task/destroy.html.erb
@@ -42,4 +43,7 @@ class TasksController < ApplicationController
         params.require(:task)permit(:tittle :details :completed)
     end 
 
+    def set_restaurant
+        @task = Task.find(params[:id])
+    end
 end
